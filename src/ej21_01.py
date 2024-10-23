fe9a402a3ec33d1c1201ff29f20a5b09f1e5ef44
mtamrod534 <trmario@iesrafaelalberti.es>
2024-10-23 17:24:47 +0200

    Ej 01 y 06

---
+++ src/ej21_01.py
@@ -0,0 +1,7 @@
+"""Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no."""
+def
+
+def main():
+
+if __name__ == "__main__":
+    main()
---
+++ src/ej21_06.py
@@ -0,0 +1,59 @@
+
+def comprobar_nombre(nombre: str):
+    try:
+        if nombre.count('.') > 0 or nombre[0].isdigit() or float(nombre):
+            raise Exception("El nombre debe de ser una cadena de caracteres válida")
+    except ValueError:
+        pass
+
+
+def comprobar_sexo(sexo: str):
+    if sexo != "FEMENINO" and sexo != "MASCULINO":
+        raise ValueError("Debes introducir un sexo válido")
+    
+
+def comprobar_sexo_nombre(sexo: str, nombre: str):
+    try:
+        comprobar_sexo(sexo)
+        comprobar_nombre(nombre)
+    except (ValueError, Exception) as e:
+        print(f"*ERROR* {e}")
+        return True
+        
+    return False
+
+
+def rango(sexo: str, nombre: str) -> str:
+    grupo = ""
+    primera_letra = nombre[0]
+
+    if sexo == "FEMENINO":
+        if primera_letra < "M":
+            grupo = "A"
+        else:
+            grupo = "B"
+    else:
+        if primera_letra >= "M":
+            grupo = "A"
+        else:
+            grupo = "B"
+
+    return grupo
+
+
+def main():
+    error = True
+
+    while error:
+        sexo = input("Introduce tu sexo (masculino/femenino): ").upper()
+        nombre = input("Introduce tu nombre: ").upper()
+
+        error = comprobar_sexo_nombre(sexo, nombre)
+        
+    grupo = rango(sexo, nombre)
+
+    print(f"Eres del sexo {sexo.lower()}, te llamas {nombre.capitalize()} y perteneces al grupo {grupo}")
+
+if __name__ == "__main__":
+    main()
+
