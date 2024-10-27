5fbd9046ca5e1ae07f36e6e5423be81acd8dc0d4
mtamrod <trmarioiesrafaelalberti.es>
2024-10-27 13:51:44 +0100

    Subida 1ª parte

--- src/ej21_07.py
+++ src/ej21_07.py
@@ -4,36 +4,43 @@ def comprobar_renta():
     renta = input("Introduce tu renta anual: ")
     error = True
     try:
-        float(renta)
+        renta = float(renta)
         error = False
+
+        if renta <= 0:
+            raise ValueError("Los valores negativos no están permitidos")
         
     except ValueError as e:
         if error:
             print("*ERROR* El valor introducido no es un número")
         else:
             print(f"*ERROR* {e}")
+    
+    return renta
 
 
-def tipo_impositivo():
-    renta = 10000
+def tipo_impositivo(renta):
     if  renta < 10000 and renta > 0:
         return 5
     elif renta >= 10000 and renta < 20000:
         return 15
-    elif renta > 20000 and renta < 35000:
+    elif renta >= 20000 and renta < 35000:
         return 20
-    elif renta > 35000 and renta < 60000:
+    elif renta >= 35000 and renta < 60000:
         return 30
     elif renta >= 60000:
         return 45
-    else:
-        raise ValueError("Los valores negativos no están permitidos")
 
 
 def main():
-    comprobar_renta()
-    tipo_impositivo(renta)
+    renta = comprobar_renta()
+    tipo = tipo_impositivo(renta)
     
+    if tipo is None:
+        print(f"Su tipo impositivo es INVÁLIDO")
+    else:
+        print(f"Su tipo impositivo es {tipo}%")
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
     main()
